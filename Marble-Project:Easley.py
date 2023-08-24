7c4688a52c963752e0db0d2da70f6267a19dd61c
James Green <jpgreen099@gmail.com>
2023-08-24 17:42:30 +0000

    Marble-Project:Easley

---
+++ Marble-Project:Easley.py
@@ -0,0 +1,5 @@
+print("Welcome to the Space Travel Calculator")
+distance = float(input("enter the distance to the celestial object in light-years"))
+speed = float(input("enter spacecraft speed"))
+time = distance / speed
+print("It will take", time, "lightyears")
